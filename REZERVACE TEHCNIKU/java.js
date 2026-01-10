744765f02cfefd8ebdfedda4c84b50a4183cf586
Ufon55 <denisnekuda55@gmail.com>
2026-01-10 17:54:17 +0100

    muzu ji mzpatky

--- REZERVACE TEHCNIKU/java.js
+++ REZERVACE TEHCNIKU/java.js
@@ -1,5 +1,9 @@
 const rezervaceT = document.getElementById("rezervaceT")
 const rezervaceM = document.getElementById("rezervaceM")
+const Ondrej = document.getElementById("Ondrej")
+const Richard = document.getElementById("Richard")
+const Lukas = document.getElementById("Lukas")
+const Martin = document.getElementById("Martin")
 
 const technici = document.querySelector(".technici")
 const mistnosti = document.querySelector(".mistnosti")
@@ -10,7 +14,31 @@ rezervaceT.addEventListener("click", () => {
     technici.classList.add("show")
 })
 
-rezervaceM.addEventListener("click",() => {
+rezervaceM.addEventListener("click", () => {
     shown.classList.add("blur")
     mistnosti.classList.add("show")
 })
+
+
+
+Ondrej.addEventListener("click", () => {
+    shown.classList.remove("blur")
+    technici.classList.remove("show")
+
+})
+
+Richard.addEventListener("click", () => {
+    shown.classList.remove("blur")
+    technici.classList.remove("show")
+})
+
+Lukas.addEventListener("click", () => {
+    shown.classList.remove("blur")
+    technici.classList.remove("show")
+})
+
+Martin.addEventListener("click", () => {
+    shown.classList.remove("blur")
+    technici.classList.remove("show")
+})
+
