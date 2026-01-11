356d6965ba51084ab24205cfa307eff184d59cd7
Ufon55 <denisnekuda55@gmail.com>
2026-01-11 14:27:37 +0100

    zpet nefungije ale mistnsoti jo

--- REZERVACE TEHCNIKU/java.js
+++ REZERVACE TEHCNIKU/java.js
@@ -1,11 +1,17 @@
 const rezervaceT = document.getElementById("rezervaceT")
 const rezervaceM = document.getElementById("rezervaceM")
+
 const Ondrej_button = document.getElementById("Ondrej")
 const Richard_button = document.getElementById("Richard")
 const Lukas_button = document.getElementById("Lukas")
 const Martin_button = document.getElementById("Martin")
 
-const zpetButtons = document.querySelectorAll(".Zpet")
+const UC1_button = document.getElementById("UC1")
+const UC2_button = document.getElementById("UC2")
+const UC3_button = document.getElementById("UC3")
+const UC4_button = document.getElementById("UC4")
+
+const zpetButtons = document.getElementById("btnZpet")
 
 const technici = document.querySelector(".technici")
 const mistnosti = document.querySelector(".mistnosti")
@@ -51,6 +57,35 @@ Martin_button.addEventListener("click", () => {
     document.getElementById("4").textContent = "Ne"
     Martin_button.disabled = true
 })
+
+UC1_button.addEventListener("click", () => {
+    shown.classList.remove("blur")
+    mistnosti.classList.remove("show")
+    document.getElementById("5").textContent = "Ne"
+    UC1_button.disabled = true
+})
+
+UC2_button.addEventListener("click", () => {
+    shown.classList.remove("blur")
+    mistnosti.classList.remove("show")
+    document.getElementById("6").textContent = "Ne"
+    UC2_button.disabled = true
+})
+
+UC3_button.addEventListener("click", () => {
+    shown.classList.remove("blur")
+    mistnosti.classList.remove("show")
+    document.getElementById("7").textContent = "Ne"
+    UC3_button.disabled = true
+})
+
+UC4_button.addEventListener("click", () => {
+    shown.classList.remove("blur")
+    mistnosti.classList.remove("show")
+    document.getElementById("8").textContent = "Ne"
+    UC4_button.disabled = true
+})
+
 
 zpetButtons.addEventListener("click", () => {
     shown.classList.remove("blur")
